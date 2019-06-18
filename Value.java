3427508f7cf2a74c689a15ead57ea52621d8b460
GavinZ0316 <31775654+GavinZ0316@users.noreply.github.com>
2019-06-18 13:33:24 -0400

    Create Value.java

---
+++ Value.java
@@ -0,0 +1,10 @@
+/****************************************************************************
+ * Created by: Gavin Zhou
+ * Created on: May 2018
+ * Created for: assignment6
+ * This program is game BlackJack'Card value
+ ****************************************************************************/
+
+public enum Value{
+	TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING,ACE
+}
