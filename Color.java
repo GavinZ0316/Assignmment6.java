d472aeb88b8006b3a42dacf4735d338dc35f445c
GavinZ0316 <31775654+GavinZ0316@users.noreply.github.com>
2019-06-18 13:32:35 -0400

    Create Color.java

---
+++ Color.java
@@ -0,0 +1,10 @@
+/****************************************************************************
+ * Created by: Gavin Zhou
+ * Created on: May 2018
+ * Created for: assignment6
+ * This program is game BlackJack'Card Color
+ ****************************************************************************/
+
+public enum Color {
+	DIMAOND, CLUB, SPADE, HEART
+}
